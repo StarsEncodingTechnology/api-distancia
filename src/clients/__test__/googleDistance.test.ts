44d2546933872198f7c9a5e5c8f6caabd08d034b
Pedro Elias Costa Santos <54581042+PedroEliasCS@users.noreply.github.com>
2022-07-29 15:54:39 -0300

    testando client

--- src/clients/__test__/googleDistance.test.ts
+++ src/clients/__test__/googleDistance.test.ts
@@ -1,23 +1,64 @@
 import { GoogleDistance } from "../googleDistance";
-import respostaEsperado from "@test/fixtures/resposta_distancia.json"
+import respostaEsperado from "@test/fixtures/resposta_distancia.json";
+import * as HTTPUtil from "@src/util/request";
+import respostaGoogleCorreta from "@test/fixtures/resposta_API_google_distance_matrix.json";
+import respostaGoogleSemDistancia from "@test/fixtures/respostaSemResultado_google_matrix.json"
+
+jest.mock("@src/util/request");
+// isso é pra mokar o axios
+// o axios é a ferramentas de contato com  a api
+// mock é a ferramenta para simulação de obj
+// que serve para o teste sem necessidade de conexão real
+// com API
 
 describe("GoogleDistance client", () => {
-    it("deve retornar os dados normalizados vindo da api do google", async () => {
-        const origem = {
-            cidade: "franca",
-            uf: "sp"
-        };
-        const destino = {
-            cidade: "cristais paulista",
-            uf: "sp"
-        };
+  const MockedRequestClass = HTTPUtil.Request as jest.Mocked<
+    typeof HTTPUtil.Request
+  >;
+  // da ao MockedRequestClass o valor da classe HTTPUtil sendo do tipo 
+
+  const mockedRequest = new HTTPUtil.Request() as jest.Mocked<HTTPUtil.Request>;
+
+  it("deve retornar os dados normalizados vindo da api do google", async () => {
+    const origem = {
+      cidade: "Franca",
+      uf: "SP",
+    };
+    const destino = {
+      cidade: "Cristais Paulista",
+      uf: "SP",
+    };
+
+    mockedRequest.get.mockResolvedValue({
+        data: respostaGoogleCorreta
+    } as HTTPUtil.Response)
+
+    const googleDistance = new GoogleDistance(mockedRequest);
+    const response = await googleDistance.buscaDistancia(origem, destino);
+
+    expect(response).toEqual(respostaEsperado);
+  });
+
+  // it("deve retornar um erro se a GoogleMatrix retornar sem resultados",async () => {
+  //   const origem = {
+  //     cidade: "Franca",
+  //     uf: "SP",
+  //   };
+  //   const destino = {
+  //     cidade: "Cristais Paulista",
+  //     uf: "SP",
+  //   };
+
+  //   mockedRequest.get.mockResolvedValue({
+  //     data: respostaGoogleSemDistancia
+  //   } as HTTPUtil.Response)
 
 
-        const googleDistance = new GoogleDistance();
-        const response = googleDistance.buscaDistancia(origem, destino)
-        console
-        expect(response).toEqual({})
+  //   const googleDistance = new GoogleDistance(mockedRequest);
+  //   const response = await googleDistance.buscaDistancia(origem, destino);
 
-        
-    })
+  //   await expect(response).rejects.toThrow('asd')
+
+  // })
+
 });
--- src/clients/googleDistance.ts
+++ src/clients/googleDistance.ts
@@ -1,17 +1,95 @@
+import * as HTTPUtil from "@src/util/request";
+import config, { IConfig } from "config";
 
+const googledistancematrix: IConfig = config.get(
+  "App.resources.googleDistancematrix"
+);
 
 export interface Destino {
-    cidade: string;
-    uf: string;
+  cidade: string;
+  uf: string;
 }
 
 export interface Origem extends Destino {}
 
+export interface DadosCorretosGoogleMatrix {
+  origem: Destino;
+  destino: Origem;
+  distancia: number;
+}
+
+export interface DistanceMatrix {
+  text: string;
+  value: number;
+}
+
+export interface DurationMatrix extends DistanceMatrix {}
+
+export interface ElementsDistanciaMatrix {
+  distance: DistanceMatrix;
+  duration: DurationMatrix;
+  status: string;
+}
+
+export interface RowsElementsDistanciaMatrix {
+  elements: ElementsDistanciaMatrix[];
+}
+
+export interface ResponseGoogleMatrix {
+  destination_adresses: Array<string>;
+  origin_adresses: Array<string>;
+  rows: RowsElementsDistanciaMatrix[];
+  status: string;
+}
 
 export class GoogleDistance {
+  constructor(protected request: HTTPUtil.Request) {}
 
-    public async buscaDistancia(destino: Destino, origem: Origem): Promise<{}>{
-        return Promise.resolve({})
-    }
+  public async buscaDistancia(
+    origem: Origem,
+    destino: Destino
+  ): Promise<DadosCorretosGoogleMatrix> {
+    const variavelDestino = destino.cidade + "-" + destino.uf;
+    const variavelOrigem = origem.cidade + "-" + origem.uf;
 
+    const response = await this.request.get<ResponseGoogleMatrix>(`
+        ${googledistancematrix.get(
+          "apiUrl"
+        )}/json?origens=${variavelDestino}&destinations=${variavelOrigem}&key=${googledistancematrix.get(
+      "apiToken"
+    )}  
+    `);
+
+    return this.normalizaDados(response.data, destino, origem);
+  }
+
+  private normalizaDados(
+    distancia: ResponseGoogleMatrix,
+    destino: Destino,
+    origem: Origem
+  ): DadosCorretosGoogleMatrix {
+    const valorDistance = distancia.rows[0].elements.find(
+      (element) => !!element?.distance?.text
+    )?.distance.text;
+
+    // const valorDistanciaNumber: number = Number.parseFloat(
+    //   valorDistance?.split(" ")[0] || "error"
+    // );
+
+    return {
+      destino: {
+        cidade: destino.cidade,
+        uf: destino.uf,
+      },
+      origem: {
+        cidade: origem.cidade,
+        uf: origem.uf,
+      },
+      distancia: 21,
+    };
+  }
+
+  private validaDistancia(distanciaDados: ElementsDistanciaMatrix): Boolean {
+    return !!distanciaDados?.["distance"];
+  }
 }
