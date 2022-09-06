bacc248ed7e2bbf6e84004a4c4476ffe682af499
Pedro Elias Costa Santos <54581042+PedroEliasCS@users.noreply.github.com>
2022-09-06 16:31:43 -0300

    pequenas mudanças

--- src/index.ts
+++ src/index.ts
@@ -25,6 +25,8 @@ process.on("uncaughtException", (error) => {
 (async () => {
   // parte inicial de tudo
   try {
+    logger.info((config.get("App.port")));
+    return
     const server = new SetupServer(config.get("App.port"));
     await server.init();
     server.start();
